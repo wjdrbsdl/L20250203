6f1a0bf83cd36c852c15f6e06960fb4a42e21d7d
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-02-03 12:00:55 +0900

    학원
    학원

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World! 집");
+            Console.WriteLine("Hello, World! 학원");
         }
     }
 }
